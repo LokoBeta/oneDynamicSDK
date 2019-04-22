0689f00ce5d5070ac7f6d8b859be4842ed497a27
LLH <luolinhao@polyv.net>
2019-04-22 15:21:48 +0800

    创建版本 0.0.1 首次提交

---
+++ oneDynamicSDK/oneDynamicClassHello.m
@@ -0,0 +1,21 @@
+//
+//  oneDynamicClassHello.m
+//  oneDynamicSDK
+//
+//  Created by Lincal on 2019/4/19.
+//  Copyright © 2019 polyv. All rights reserved.
+//
+
+#import "oneDynamicClassHello.h"
+
+//#import <subDynamicSDK/subDynamicSDK.h>
+
+@implementation oneDynamicClassHello
+
++ (void)hello{
+    NSLog(@"POLYVTEST - I am in oneFramewrok");
+    
+//    [subDynamicClassHello hello];
+}
+
+@end
